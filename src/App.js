39edfc12d8410b176239365a8495dfc98a876919
Oluwaseyi Meshileya <seyimeshileya@gmail.com>
2023-04-10 23:08:20 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NewExpense from './component/NewExpense/NewExpense';
+import Expenses from "./component/Expenses/Expenses"
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)car insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+  ];
+
+  const addExpenseHandler = expense => {       //
+    console.log('In App.js');
+    console.log(expense);
+  };
+
+  // return React.createElement('div', {}, React.createElement('h2', {}, "Let's get started"), React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ src/component/Expenses/ExpenseDate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ExpenseDate.css';
+
+const ExpenseDate =(props) => {
+  const month = props.date.toLocaleString('en-Us', { month: 'long' });
+  const day = props.date.toLocaleString('en-Us', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{day}</div>
+      <div className="expense-date__year">{month}</div>
+      <div className="expense-date__day">{year}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
