58cd30788535092aa2c8b2b72e4f218daee14ff0
Oluwaseyi Meshileya <seyimeshileya@gmail.com>
2023-04-19 22:17:10 +0100

    Expenses can be saved

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NewExpense from './component/NewExpense/NewExpense';
-import Expenses from "./component/Expenses/Expenses"
+import Expenses from "./component/Expenses/Expenses";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)car insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)car insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-  ];
+  const [expenses, setExpenses] =useState(DUMMY_EXPENSES);
 
-  const addExpenseHandler = expense => {       //
-    console.log('In App.js');
-    console.log(expense);
+  const addExpenseHandler = (expense) => {    
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    }); 
   };
+
+  // here, we intend to set our expense array to new array which include the new expense:line35
+  
 
   // return React.createElement('div', {}, React.createElement('h2', {}, "Let's get started"), React.createElement(Expenses, { items: expenses })
   // );
--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -1,51 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
-import ExpensesFilter from './ExpensesFilter';
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
-
-
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filterChangeHandler = selectYear => {
+  const filterChangeHandler = (selectYear) => {
     setFilteredYear(selectYear);
   };
 
   return (
     <div>
-
       <Card className="expenses">
-        
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            keys={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+        ;
+        {/*this expense is passed as a perameter into this function, automatically the expense is used to attract the title, amount and the date*/}
       </Card>
-    </div> 
+    </div>
   );
-}
+};
 
 export default Expenses;
